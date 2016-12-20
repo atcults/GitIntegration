d6782b4e8e13c14938077e3f02445890fbdcf8fc
sanelib <sunny@sanelib.com>
2016-12-20 12:09:27 +0530

    Adding test case for git log

--- test/IntegrationTest/GitLogTest.cs
+++ test/IntegrationTest/GitLogTest.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using System;
+using GitIntegration;
+using NLog;
+using Xunit;
 
 namespace IntegrationTest
 {
@@ -7,7 +10,30 @@ namespace IntegrationTest
         [Fact]
         public void GitLogShouldGetFullLogs()
         {
-            Assert.True(true);
+            Console.WriteLine("Getting configuration");
+
+            var configuration = ProjectConfigurationProvider.Instance;
+
+            var ProjectDetail = configuration.Projects[0];
+
+            LogHelper.AddLogger(ProjectDetail.Name);
+
+            var logger = LogManager.GetLogger(ProjectDetail.Name);
+
+            logger.Info($"Project Name {ProjectDetail.Name} and Location is {ProjectDetail.Location}");
+
+            var output = GitParser.ListShaWithFiles(ProjectDetail);
+
+            foreach (var elm in output)
+            {
+                logger.Info(elm.ToString());
+            }
+
+            Assert.True(output.Count > 0);
+
+            LogManager.Flush();
+            LogManager.Shutdown();
+            
         }
     }
 }
